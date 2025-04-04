5b1f3bbe09527da1cadc1033fae75743aaf389c6
Michelle <mpuschkarow@web.de>
2025-04-04 10:55:36 +0200

    input values saved with array method

--- !liqui-planner/src/js/main.js
+++ !liqui-planner/src/js/main.js
@@ -11,53 +11,65 @@ const accounting = {
         totalCosts: 0,
         sum: 0,
     },
+    inputs: [],
     save_input(){
-        this.new_input.title = prompt("Titel:", "z.B. Einkauf");
-        this.new_input.type = prompt("Typ (Einnahme o. Ausgabe):");
-        this.new_input.amount = parseInt(prompt("Betrag (in Cent):", "z.B. 10.42 € = 1042 Ct"));
-        this.new_input.date = prompt("Datum:", "jjjj-mm-tt");
-    },
-    give_input(){
-        console.log(
-            `Titel: ${this.new_input.title}
-             Typ: ${this.new_input.type}
-             Betrag (in Ct): ${this.new_input.amount} Cent
-             Datum (jjjj-mm-tt): ${this.new_input.date}`
+        this.inputs.push(
+            {
+                title: prompt("Titel:", "z.B. Einkauf"),
+                type: prompt("Typ (Einnahme o. Ausgabe):"),
+                amount: parseInt(prompt("Betrag (in Cent):", "z.B. 10.42 € = 1042 Ct")),
+                date: prompt("Datum:", "jjjj-mm-tt"),
+            }
         );
     },
-    input_calculated_with_sum (){
-        switch (this.new_input.type){
-            case "Einnahme":
-                this.balance.totalIncome += this.new_input.amount;
-                this.balance.sum += this.new_input.amount;
-                break;
-            case "Ausgabe":
-                this.balance.totalCosts += this.new_input.amount;
-                this.balance.sum -= this.new_input.amount;
-                break;
-            default:
-                console.log(`Eingabe ${this.new_input.type} kann nicht zugeordnet werden. Bitte nutze Einnahme oder Ausgabe`);
-        }
-    },
-    give_sum (){
-        console.log(
-            `Einnahmen: ${this.balance.totalIncome} ct
-             Ausgaben: ${this.balance.totalCosts} ct
-             Bilanz: ${ this.balance.sum} ct
-             Bilanz ist positiv: ${ this.balance.sum >= 0}`
-        );
-    },
+    // save_input(){
+    //     this.new_input.title = prompt("Titel:", "z.B. Einkauf");
+    //     this.new_input.type = prompt("Typ (Einnahme o. Ausgabe):");
+    //     this.new_input.amount = parseInt(prompt("Betrag (in Cent):", "z.B. 10.42 € = 1042 Ct"));
+    //     this.new_input.date = prompt("Datum:", "jjjj-mm-tt");
+    // },
+    // give_input(){
+    //     console.log(
+    //         `Titel: ${this.new_input.title}
+    //          Typ: ${this.new_input.type}
+    //          Betrag (in Ct): ${this.new_input.amount} Cent
+    //          Datum (jjjj-mm-tt): ${this.new_input.date}`
+    //     );
+    // },
+    // input_calculated_with_sum (){
+    //     switch (this.new_input.type){
+    //         case "Einnahme":
+    //             this.balance.totalIncome += this.new_input.amount;
+    //             this.balance.sum += this.new_input.amount;
+    //             break;
+    //         case "Ausgabe":
+    //             this.balance.totalCosts += this.new_input.amount;
+    //             this.balance.sum -= this.new_input.amount;
+    //             break;
+    //         default:
+    //             console.log(`Eingabe ${this.new_input.type} kann nicht zugeordnet werden. Bitte nutze Einnahme oder Ausgabe`);
+    //     }
+    // },
+    // give_sum (){
+    //     console.log(
+    //         `Einnahmen: ${this.balance.totalIncome} ct
+    //          Ausgaben: ${this.balance.totalCosts} ct
+    //          Bilanz: ${ this.balance.sum} ct
+    //          Bilanz ist positiv: ${ this.balance.sum >= 0}`
+    //     );
+    // },
     add_input(){
         this.save_input();
-        this.give_input();
-        this.input_calculated_with_sum();
-        this.give_sum();
+        // this.give_input();
+        // this.input_calculated_with_sum();
+        // this.give_sum();
     }
 }
 
 //Kann die Eingabe auf diese Weise mehrfach wiederholen
 accounting.add_input();
 accounting.add_input();
-accounting.add_input();
+console.log(accounting);
+// accounting.add_input();
 
 
