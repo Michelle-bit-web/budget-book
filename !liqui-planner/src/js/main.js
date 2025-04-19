70ecde6e106a1bef7e9480af7a425f649e270c9a
Michelle <mpuschkarow@web.de>
2025-04-19 13:44:05 +0200

    show negative value for negative balance

--- !liqui-planner/src/js/main.js
+++ !liqui-planner/src/js/main.js
@@ -197,14 +197,15 @@ show_inputs(){
     sum_row.insertAdjacentElement("afterbegin", sum_title);
     let sum_amount = document.createElement("span");
     if(this.balance.get("sum") >= 0){
-      sum_amount.setAttribute("class", "positiv")
+      sum_amount.setAttribute("class", "positiv");
+      sum_amount.textContent = `${(this.balance.get("sum") / 100).toFixed(2).replace(/\./, ",")} €`;
     }else if(this.balance.get("sum") < 0){
-      sum_amount.setAttribute("class", "negativ")
+      sum_amount.setAttribute("class", "negativ");
+      sum_amount.textContent = `-${(this.balance.get("sum") / 100).toFixed(2).replace(/\./, ",")} €`;
     };
-    sum_amount.textContent = `${(this.balance.get("sum") / 100).toFixed(2).replace(/\./, ",")} €`;
     sum_row.insertAdjacentElement("beforeend", sum_amount);
-
     sum.insertAdjacentElement("beforeend", sum_row);
+    
     return sum;
   },
   show_sum(){
