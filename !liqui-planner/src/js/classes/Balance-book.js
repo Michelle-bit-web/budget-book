b76d5535b8fa53fbd82a757304e5e103caa7ac86
Michelle <mpuschkarow@web.de>
2025-04-22 17:12:50 +0200

    localStorage added to balance book class

--- !liqui-planner/src/js/classes/Balance-book.js
+++ !liqui-planner/src/js/classes/Balance-book.js
@@ -4,15 +4,17 @@ class Balancebook{
     constructor(){
         this._inputs = [];
         this._navigation = new Navigationlist();
-        this.formular = new Formular();
+        this._formular = new Formular();
         this._all_month_list = new ListAllMonth();
         this._balance = new Balance(); 
+        this._get_saved_date();
     }
-  add_input(formular_data) {
-    let new_input = new Inputs(formular_data.title, formular_data.amount, formular_data.type, formular_data.date);
+  add_input(data) {
+    let new_input = new Inputs(data.title, data.amount, data.type, data.date);
     this._inputs.push(new_input);
     this._all_month_list.refresh(this._inputs);
     this._balance._calculate_sum(this._inputs);
+    this._save_data();
   }
   remove_input(timestamp){
     let start_index;
@@ -25,11 +27,28 @@ class Balancebook{
     this._inputs.splice(start_index, 1);
     this._all_month_list.refresh(this._inputs);
     this._balance._calculate_sum(this._inputs);
+    this._save_data();
   }
   start(){
     this._navigation.show();
     this._formular.show();
     this._all_month_list.show();
     this._balance.show();
+  }
+  _save_data(){
+    localStorage.setItem("inputs", JSON.stringify(this._inputs));
+  }
+  _get_saved_date(){
+    let saved_data = localStorage.getItem("inputs");
+    if(saved_data !== null){
+      JSON.parse(saved_data).forEach(data => {
+        this.add_input({
+          title: data._title,
+          amount: data._amount,
+          type:data._type,
+          date: new Date(data._date)
+        });
+      });
+    }
   }
 };
