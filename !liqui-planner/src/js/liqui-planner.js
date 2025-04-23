0be02aecbfc022a1b5e574b76ff8fd4e3726e7c9
Michelle <mpuschkarow@web.de>
2025-04-23 08:38:05 +0200

    js files minified with javascript-minifier.com

--- !liqui-planner/src/js/liqui-planner.js
+++ !liqui-planner/src/js/liqui-planner.js
@@ -1,5 +1,12 @@
-import Balancebook from "./classes/Balance-book.js";
+/**
+ * Das Hauptmodul "liqui-planner" ist für die Instanziierung des Haushaltsbuchs
+ * und den Start der Anwendung zuständig.
+ */
+import Balancebook from "../../classes/Balance-book.js";
 
+/**
+ * Instanziierung des Haushaltsbuchs und Start der Anwendung
+ */
 let liqui_planner = new Balancebook();
 liqui_planner.start();
 
